334a42e85a15f354ba0e006a6c8ae52099e73f35
EthanLin0302 <50626674+EthanLin0302@users.noreply.github.com>
2022-12-14 16:05:03 +0800

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "Hello World!2333"
+    import os
+    return "Hello World!%s"%os.environ
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0', port=1337)
